f574d6b6b785cacb7f4afe0bf1496627d9b530e2
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-11-04 19:02:58 +0900

    処理の大枠を書いた  - ネットワークの準備以降、訓練の実行まで  - データセットとイテレータの準備以外を書いた

--- examples/iris/iris.rb
+++ examples/iris/iris.rb
@@ -1,3 +1,35 @@
 require_relative 'mlp'
 
-puts MLP.new
+# --------------- データセットの準備 -----------------
+
+# --------------- イテレータの準備 -----------------
+train_iter = nil
+valid_iter = nil
+
+# --------------- ネットワークの準備 -----------------
+predictor = MLP.new
+
+# --------------- アップデータの準備 -----------------
+net = Chainer::Links::Model::Classifier.new(predictor)
+
+optimizer = Chainer::Optimizers::MomentumSGD.new(lr: 0.01)
+optimizer.set(net)
+
+updater = Chainer::Training::StandardUpdater.new(train_iter, optimizer, device: -1) # device=-1でCPUでの計算実行を指定
+
+# --------------- トレーナの作成 -----------------
+trainer = Chainer::Training::Trainer.new(updater, stop_trigger: [30, 'epoch'], out: 'results/iris_result1')
+
+# --------------- トレーナの拡張 -----------------
+EXTENSIONS = Chainer::Training::Extensions
+trainer.extend(EXTENSIONS::Evaluator.new(valid_iter, net, device: -1), name: 'val')
+trainer.extend(EXTENSIONS::LogReport.new(trigger: [1, 'epoch'], log_name: 'log'))
+
+filename_proc = Proc.new { |t| "snapshot_epoch-#{t.updater.epoch}" }
+trainer.extend(EXTENSIONS::Snapshot.new(filename_proc: filename_proc), trigger: [1, 'epoch'])
+
+trainer.extend(EXTENSIONS::PrintReport.new(['epoch', 'iteration', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'l1/W/data/mean', 'elapsed_time']))
+trainer.extend(EXTENSIONS::ParameterStatistics(net.predictor.l1, {'mean': np.mean}, report_grads=True))
+
+# --------------- 訓練の開始 -----------------
+trainer.run
