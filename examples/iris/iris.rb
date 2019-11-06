b5a4e09dd491fa9b0bb962d220aff91548bb5aee
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-11-06 22:31:55 +0900

    fix log file name

--- examples/iris/iris.rb
+++ examples/iris/iris.rb
@@ -30,14 +30,14 @@ optimizer.setup(net)
 updater = Chainer::Training::StandardUpdater.new(train_iter, optimizer, device: -1) # device=-1でCPUでの計算実行を指定
 
 # --------------- トレーナの作成 -----------------
-trainer = Chainer::Training::Trainer.new(updater, stop_trigger: [30, 'epoch'], out: 'results/iris_result1')
+trainer = Chainer::Training::Trainer.new(updater, stop_trigger: [30, 'epoch'], out: "results/iris_result_#{Time.now.strftime("%Y%m%d_%H%M%S")}")
 
 # --------------- トレーナの拡張 -----------------
 EXTENSIONS = Chainer::Training::Extensions
 trainer.extend(EXTENSIONS::Evaluator.new(valid_iter, net, device: -1), name: 'val')
 trainer.extend(EXTENSIONS::LogReport.new(trigger: [1, 'epoch'], log_name: 'log'))
 
-filename_proc = Proc.new { |t| "snapshot_epoch-#{t.updater.epoch}" }
+filename_proc = Proc.new { |t| format("snapshot_epoch-%02d", t.updater.epoch) }
 trainer.extend(EXTENSIONS::Snapshot.new(filename_proc: filename_proc), trigger: [1, 'epoch'])
 
 trainer.extend(EXTENSIONS::PrintReport.new(['epoch', 'iteration', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'l1/W/data/mean', 'elapsed_time']))
