c526f2be54ad14e48331579494c81d57a9d5b76b
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-11-17 13:30:06 +0900

    VocabularyのidをIntegerに修正 - stringだと使い勝手が悪かった

--- spec/examples/seq2seq/utility/vocabulary_spec.rb
+++ spec/examples/seq2seq/utility/vocabulary_spec.rb
@@ -10,9 +10,9 @@ describe Examples::Seq2seq::Utility::Vocabulary do
 
   context '#word_to_id' do
     it '辞書に存在する単語に対して正しいidが返ってくる' do
-      expect(vocabulary.word_to_id('ball')).to eq '2'
-      expect(vocabulary.word_to_id('paper')).to eq '3'
-      expect(vocabulary.word_to_id('cup')).to eq '4'
+      expect(vocabulary.word_to_id('ball')).to eq 2
+      expect(vocabulary.word_to_id('paper')).to eq 3
+      expect(vocabulary.word_to_id('cup')).to eq 4
     end
 
     it '辞書に存在しない単語に対してUNK[:id]が返ってくる' do
@@ -27,13 +27,13 @@ describe Examples::Seq2seq::Utility::Vocabulary do
 
   context '#id_to_word' do
     it '辞書に存在するidに対して正しい単語が返ってくる' do
-      expect(vocabulary.id_to_word('2')).to eq 'ball'
-      expect(vocabulary.id_to_word('3')).to eq 'paper'
-      expect(vocabulary.id_to_word('4')).to eq 'cup'
+      expect(vocabulary.id_to_word(2)).to eq 'ball'
+      expect(vocabulary.id_to_word(3)).to eq 'paper'
+      expect(vocabulary.id_to_word(4)).to eq 'cup'
     end
 
     it '辞書に存在しないidに対してUNK[:word]が返ってくる' do
-      expect(vocabulary.id_to_word('99999')).to eq unknown_word[:word]
+      expect(vocabulary.id_to_word(99999)).to eq unknown_word[:word]
     end
 
     it 'クラス定数で定義したidに対して正しい単語が返ってくる' do
@@ -53,18 +53,18 @@ describe Examples::Seq2seq::Utility::Vocabulary do
 
     context '#word_to_id' do
       it '辞書に存在する単語に対して正しいidが返ってくる' do
-        expect(vocabulary.word_to_id('ball')).to eq '2'
-        expect(vocabulary.word_to_id('paper')).to eq '3'
-        expect(vocabulary.word_to_id('cup')).to eq '5'
+        expect(vocabulary.word_to_id('ball')).to eq 2
+        expect(vocabulary.word_to_id('paper')).to eq 3
+        expect(vocabulary.word_to_id('cup')).to eq 5
       end
     end
 
     context '#id_to_word' do
       it '辞書に存在するidに対して正しい単語が返ってくる' do
-        expect(vocabulary.id_to_word('2')).to eq 'ball'
-        expect(vocabulary.id_to_word('3')).to eq 'paper'
-        expect(vocabulary.id_to_word('4')).to eq unknown_word[:word]
-        expect(vocabulary.id_to_word('5')).to eq 'cup'
+        expect(vocabulary.id_to_word(2)).to eq 'ball'
+        expect(vocabulary.id_to_word(3)).to eq 'paper'
+        expect(vocabulary.id_to_word(4)).to eq unknown_word[:word]
+        expect(vocabulary.id_to_word(5)).to eq 'cup'
       end
     end
 
