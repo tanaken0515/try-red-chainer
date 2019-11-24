cba82de8bf5f7a19064d794b384699b369aa2474
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-11-24 17:05:59 +0900

    rename constants in MLP

--- examples/iris/mlp.rb
+++ examples/iris/mlp.rb
@@ -1,23 +1,23 @@
 require 'chainer'
 
 class MLP < Chainer::Chain
-  LINEAR = Chainer::Links::Connection::Linear
-  RELU = Chainer::Functions::Activation::Relu
+  Linear = Chainer::Links::Connection::Linear
+  Relu = Chainer::Functions::Activation::Relu
 
   def initialize(mid_units_size: 100, output_size: 3)
     super()
     init_scope do
-      @l1 = LINEAR.new(nil, out_size: mid_units_size)
-      @l2 = LINEAR.new(mid_units_size, out_size: mid_units_size)
-      @l3 = LINEAR.new(mid_units_size, out_size: output_size)
+      @l1 = Linear.new(nil, out_size: mid_units_size)
+      @l2 = Linear.new(mid_units_size, out_size: mid_units_size)
+      @l3 = Linear.new(mid_units_size, out_size: output_size)
     end
   end
 
   def call(x)
     u1 = @l1.(x)
-    h1 = RELU.relu(u1)
+    h1 = Relu.relu(u1)
     u2 = @l2.(h1)
-    h2 = RELU.relu(u2)
+    h2 = Relu.relu(u2)
     @l3.(h2)
   end
 end
